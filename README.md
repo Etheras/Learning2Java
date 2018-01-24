38aa7ef5372736f9342b19f953cc2378d703a926
Etheras <foxofire@hotmail.com>
2018-01-24 17:42:35 +0200

    Initial mod upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Learning2Java
+Primarily an issue tracker, maybe a mod repository for the modpack Learning2Java, whose objective is to teach the basics to java to whomever is willing.
---
+++ src/main/java/Etheras/Mods/Learning2Java/Learning2Java.java
@@ -0,0 +1,20 @@
+package Etheras.Mods.Learning2Java;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Learning2Java.MODID, version = Learning2Java.VERSION)
+public class Learning2Java
+{
+    public static final String MODID = "Learning2Java";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+    }
+}
